50104f73efe0ca2faf4b849a82376df7622f1a30
Neil <neil__ko@163.com>
2020-05-09 17:09:10 +0800

    真redis

--- src/main/java/com/cool/kai/controller/UserController.java
+++ src/main/java/com/cool/kai/controller/UserController.java
@@ -2,18 +2,24 @@ package com.cool.kai.controller;
 
 import com.cool.kai.model.User;
 import com.cool.kai.service.UserService;
+import com.cool.kai.utils.RedisUtils;
 import com.github.pagehelper.PageInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/test")
+@Slf4j
 public class UserController {
 
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private RedisUtils redisUtils;
 
     @GetMapping("/getUsers")
     @ResponseBody
@@ -25,7 +31,9 @@ public class UserController {
     @GetMapping("/findById")
     @ResponseBody
     public User findById(@RequestParam Long id){
-
+        log.info("sdf");
+        redisUtils.set("id",id);
+        System.out.println("redis缓存值:"+redisUtils.get("id"));
         return userService.findById(id);
     }
 
