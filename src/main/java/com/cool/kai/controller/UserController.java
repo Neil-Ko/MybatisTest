834b691651d172b077c4342effbf208668a671c3
Neil <neil__ko@163.com>
2020-06-22 10:29:36 +0800

    mapper.xml

--- src/main/java/com/cool/kai/controller/UserController.java
+++ src/main/java/com/cool/kai/controller/UserController.java
@@ -10,6 +10,8 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Objects;
+
 @Controller
 @RequestMapping("/test")
 @Slf4j
